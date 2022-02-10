7cbfc66b63a41b9b93492ad99d0c2ac0eb599b42
Eric <tryharder3l33t@gmail.com>
2022-02-09 21:53:26 -0500

    db working

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 8765;
 const express = require("express");
 const { reset } = require("nodemon");
 const { Pool } = require("pg");
+const fs = require("fs");
+const path = require("path");
+
+//You use the local DB with your name on it while developing so you should seed the db. This is the connnection
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   //For deployment
@@ -11,11 +15,41 @@ const pool = new Pool({
     rejectUnauthorized: false,
   },
   //For local development
-  //ssl: false,
+  //   ssl: false,
 });
+
+const seeder = async () => {
+  try {
+    //await client.connect();
+    const client = await pool.connect();
+    fs.readFile(path.join(__dirname, "seed.sql"), async (err, data) => {
+      const sql = data.toString();
+      await client.query(sql);
+      console.log("seeded");
+    });
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+seeder();
 
 express()
   .get("/", (req, res) => {
     res.send("HELLO Express");
+  })
+  .get("/db", async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM users");
+      //   const result = await client.query("SELECT * FROM test_table");
+      const results = { results: result ? result.rows : null };
+      //res.render("pages/db", results);
+      res.send(results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
   })
   .listen(PORT, () => console.log(`Express is listening on ${PORT}`));
